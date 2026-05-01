98f0b236600742db48adad972c37a29999f7ce47
mohammadAlamin99 <alamin.cse.99@gmail.com>
2026-05-01 10:02:40 +0600

    first commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,6 @@ export default function Header() {
   return (
     <div>
       <h1 className="text-2xl font-bold">My App</h1>
-      <h2 className="text-lg">Welcome to my app</h2>
     </div>
   )
 }
